47d2c57bb04c86ba9985a4f45f683686d01463d7
lfranco <larissa.jordana15@gmail.com>
2023-11-15 17:46:20 -0300

    add Makefile

---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfranco <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 14:39:37 by lfranco           #+#    #+#             */
+/*   Updated: 2023/11/15 15:16:12 by lfranco          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *XXXXX, ...);
+
+#endif
