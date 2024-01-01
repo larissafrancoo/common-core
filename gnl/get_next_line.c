798cc5d783ccd6bdb71ff8c0d1eb71bee879f981
Larissa <larissa.jordana15@gmail.com>
2024-01-01 04:22:37 -0300

    primeiro teste colocando em pratica o conhecimento adquirido para faze a gnl

---
+++ gnl/get_next_line.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfranco <lfranco@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/01 02:25:18 by lfranco           #+#    #+#             */
+/*   Updated: 2024/01/01 02:31:44 by lfranco          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*line_operator(int fd, char *strline, char *save)
+{
+	int		rd_line;
+	char	*temp;
+
+	rd_line = 1;
+	while (rdline != '\0')
+	{
+		rd_line = read(fd, strline, BUFFER_SIZE);
+		if (rd_line < 0)
+			return (0);
+		else if (rd_line == 0)
+			break ;
+		if (save == NULL)
+			save = ft_strdup("");
+		temp = save;
+		strline[rd_line] = '\0';
+		save = ft_strjoin(temp, strline);
+		free(temp);
+		temp = NULL;
+		if (ft_strchr(strline, '\n'))
+			break ;
+	}
+	return (save);
+}
+
+static char	*save_operator(char *line)
+{
+	size_t	count;
+	char	*save;
+
+	count = 0;
+	while (line[count] != '\n' && line[count] != '\0')
+		count++;
+	if (line[count] == '\0' || line[1] == '\0')
+		return (0);
+	save = ft_substr(line, count++, ft_strlen(line) - count);
+	if (*save == '\0')
+	{
+		free(save);
+		save = NULL;
+	}
+	line[count++] = '\0';
+	return (save);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	char		*strline;
+	static char	*save;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	strline = (char *) malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!strline)
+		return (0);
+	line = line_operator(fd, strline, save);
+	free(strline);
+	strline = NULL;
+	if (!line)
+		return (NULL);
+	save = save_operator(line);
+	return (line);
+}
---
+++ gnl/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfranco <lfranco@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/01 02:38:40 by lfranco           #+#    #+#             */
+/*   Updated: 2024/01/01 04:07:42 by lfranco          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fnctl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42000
+# endif
+
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+
+#endif
