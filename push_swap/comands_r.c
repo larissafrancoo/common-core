7ae2be4af3692c2d1b5e5adcd616c27cbb7a896c
Larissa <larissa.jordana15@gmail.com>
2024-04-23 19:44:20 -0300

    63%

---
+++ push_swap/comands_r.c
@@ -0,0 +1,43 @@
+#include "push_swap.h"
+
+void	ra(t_node **stack_a)
+{
+	rotate(stack__a);
+	ft_putendl_fd("ra", 1);
+}
+
+void	rb(t_node **stack_b)
+{
+	rotate(stack_b);
+	ft_putendl_fd("rb", 1);
+}
+
+void	rr(t_node **stack_a, t_node **stack_b)
+{
+	if ((stk_size(stack_a) < 2) || (stk_size(stack_b) < 2))
+		return ;
+	rotate(stack_a);
+	rotate(stack_b);
+	ft_putendl_fd("rr", 1);
+}
+
+void	rra(t_node **stack_a)
+{
+	rev_rotate(stack_a);
+	ft_putendl_fd("rra", 1);
+}
+
+void	rrb(t_node **stack_b)
+{
+	rev_rotate(stack_b);
+	ft_putendl_fd("rrb", 1);
+}
+
+void	rrr(t_node **stack_a, t_node **stack_b)
+{
+	if ((stk_size(stack_a) < 2) || (stk_size(stack_b) < 2))
+		return ;
+	rev_rotate(stack_a);
+	rev_rotate(stack_b);
+	ft_putendl_fd("rrr", 1);
+}
---
+++ push_swap/comands_sp.c
@@ -0,0 +1,34 @@
+#include "push_swap.h"
+
+void	sa(t_node **stack_a)
+{
+	swap(stack_a);
+	ft_putendl_fd("sa", 1);
+}
+
+void	sb(t_node **stack_b)
+{
+	swap(stack_b);
+	ft_putendl_fd("sb", 1);
+}
+
+void	ss(t_node **stack_a, t_node **stack_b)
+{
+	if ((stk_size(*stack_a) < 2) || (stk_size(*stack_b) < 2))
+		return ;
+	swap(stack_a);
+	swap(stack_b);
+	ft_putendl_fd("ss", 1);
+}
+
+void	pa(t_node **stack_b, t_node **stack_a)
+{
+	push(stack_b, stack_a);
+	ft_putendl_fd("pa", 1);
+}
+
+void	pb(t_node **stack_a, t_node **stack_b)
+{
+	push(stack_a, stack_b);
+	ft_putendl_fd("pb", 1);
+}
